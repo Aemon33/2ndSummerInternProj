a516fe5c36892dd6bfa8e380b400dc454fd37833
aemon <khazriaymen93@gmail.com>
2024-08-14 18:32:05 +0200

    do SW to List properties on the front page (static)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { Proza_Libre } from "next/font/google";
 import Categories from "./components/Categories";
-
+import PropertyList from "./components/properties/PropertyList"
 export default function Home() {
 
   return (
@@ -7,6 +8,10 @@ export default function Home() {
 
     <menu className="max-w-[1000px] mx-auto mt-52 px-6 ">
       <Categories />
+
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        <PropertyList/>
+      </div>
     </menu>
     
   );
