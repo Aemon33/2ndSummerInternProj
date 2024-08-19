d2df8630e8ee2af790bbb169f36bd2314ce042b3
aemon <khazriaymen93@gmail.com>
2024-08-19 16:10:03 +0200

    make a sw with user menu popup

---
+++ app/components/navbar/UserNav.tsx
@@ -0,0 +1,35 @@
+'use client';
+import MenuLink from "./MenuLink";
+import { useState } from "react";
+
+function UserNav() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div className=" bg-red-300  p-2 relative md:inline-block border  rounded-full cursor-pointer hover:shadow-lg transition">
+        <button className="flex items-center p-1 "> 
+        <svg  className='mr-4'  x="0px" strokeWidth="" y="0px" width="20" height="20" viewBox="0 0 50 50">
+        <path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"></path>
+        </svg>
+        <svg  fill="#000"  viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+        </svg>
+
+        </button>
+        {isOpen&&(
+          <div className="z-20 flex flex-col cursor-pointer w-[220px] absolute  top-[60px] right-0 bg-white-200 border rounded-xl shadow-md " >
+            <MenuLink
+              label="Log in" 
+              onClick={()=>alert('ti amo')}
+            />
+            <MenuLink
+              label="Sign up"
+              onClick={()=>alert('ti amo')}
+            />
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default UserNav
