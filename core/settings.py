f932d771c4559646bca0e01a5f9e6c4dadd59331
aemon <khazriaymen93@gmail.com>
2024-08-23 15:38:39 +0200

    make a lot of confs about jwt,drf, cors

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 from pathlib import Path
 
@@ -19,6 +20,50 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL = 'useraccount.User'
 
+SIDE_ID = 1
+
+WEBSITE_URL = 'http:localhost:8000'
+
+
+SIMPLE_JWT =  {
+    "ACCESS_TOKEN_LIFETIME":timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME":timedelta(days=7),
+    "ROTATE_REFRESH_TOKEN":False,
+    "BLACKLIST_AFTER_ROTATION":False,
+    "UPDATE_LAST_LOGIN":True,
+    "SIGNING_KEY":"accomplexkey",
+    "ALGORITHM":"H5512", # from GPT HS256
+        
+}
+# indicating that the username field is not used.
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+ACCOUNT_EMAIL_VERIFICATION = None
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES":(
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    "DEFAULT_PERMISSION_CLASSES":(
+        'rest_framework.permissions.IsAuthenticated'
+    )
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+]
+
+REST_AUTH = {
+    "USE_JWT":True,
+    "JWT_AUTH_HTTPONLY":False
+}
+
+
+
 
 # Application definition
 
@@ -29,6 +74,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    'allauth',
+    'allauth.account',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+    'corsheaders',
+    'property',
     'useraccount',
 
 ]
@@ -36,6 +90,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
