4d26d903deff6d913de1cfd02ccb4a21a00d5a68
aemon <khazriaymen93@gmail.com>
2024-08-25 16:02:07 +0200

    get properties in the front end and list them

--- app/components/properties/PropertyList.tsx
+++ app/components/properties/PropertyList.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+'use client';
+
+import {useEffect, useState} from 'react';
 import PropertyListItem from './PropertyListItem'
+
+export type PropertyType = {
+  id:string;
+  title:string;
+  price_per_night:number
+  image_url:string;
+} 
+
 function PropertyList() {
+const [properties, setProperties] = useState<PropertyType[]>([])
+  const getProperties  = async () =>{
+
+    const url = 'http://localhost:8000/api/properties/';
+    await fetch(url, { method:"GET"})
+    .then(res=> res.json())
+    .then(data =>{
+
+
+      setProperties(data.data) 
+      console.log('data', data)
+    } 
+   )
+      
+    .catch((error)=>{
+      console.log("error", error);
+    })
+  }
+
+
+  useEffect(()=>{
+      getProperties();
+  }, []);
+  console.log(properties)
   return (
     <>
-    <PropertyListItem/>
-    <PropertyListItem/>
-    <PropertyListItem/>
+    {properties.map(property=>{
+      return (<PropertyListItem 
+        key = {property.id}
+        property={property}
+      />)
+    })}
+    
     </>
   )
 }
