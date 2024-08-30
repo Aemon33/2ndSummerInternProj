a2e7a1cebf6205f79fdac6089fa0087a4af9f90e
aemon <khazriaymen93@gmail.com>
2024-08-30 18:53:40 +0200

    Book property is done

--- property/admin.py
+++ property/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Property
+from .models import *
 # Register your models here.
 
 admin.site.register(Property)
+admin.site.register(Reservation)
--- property/serializers.py
+++ property/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Property 
+from .models import *
 from useraccount.serializers import UserDetailSerializer
 
 
@@ -32,6 +32,16 @@ class PropertiesDetailSerializer(serializers.ModelSerializer):
             'bathrooms',
             'guests',
             'landlord',
+        )
+
+
+class ReservationsListSerializer(serializers.ModelSerializer):
+    property = PropertiesListSerializer(read_only=True, many=False)
+    
+    class Meta:
+        model = Reservation
+        fields = (
+            'id', 'start_date', 'end_date', 'number_of_nights', 'total_price', 'property'
         )
 
     
