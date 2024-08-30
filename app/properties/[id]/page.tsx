066dfe454513b675279511d6167921f7e0c6d620
aemon <khazriaymen93@gmail.com>
2024-08-30 16:23:42 +0200

    detail page for properties

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import ReservationSideBar from "@/app/components/properties/ReservationSideBar";
-const  PropertyDetailPage = () => {
+
+import apiService from "@/app/services/apiService";
+
+
+const  PropertyDetailPage = async  ({params}:{params:{id:string}}) => {
+    const property  = await apiService.get(`/api/properties/${params.id}`)
   return (
 
     <main className="max-w-[1000px] mx-auto mt-52 px-6 pb-6 ">
@@ -9,39 +14,47 @@ const  PropertyDetailPage = () => {
         <Image 
          fill
          src='/BeachHouse.jpg'
-         className="object-cover  h-full w-full"
+         className="object-cover h-full w-full"
          alt="beach house"
         />
     </div>
     <div className=" grid grid-cols-1 md:grid-cols-5 gap-4 ">
         <div className="py-6 pr-6 col-span-3">
             <h1 className="mb-4 text-4xl">
-                Property name
+                {property.title}
             </h1>
             <span className="mb-6 block text-lg text-gray-600">
-                4 guests - 2 bedrooms - 1 bathroom
+            {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathroom
             </span>
 
             <hr />
             <div className="flex py-6 items-center space-x-4">
+            {property.landlord.avatar_url && 
+            
+            (
                 <Image 
                 
-                src='/MaleProPic.jpg'
+                src={property.landlord.avatar_url}
                 className="rounded-full"
                 alt="user name"
                 width={50}
                 height={50}
                 />
+            )
+            }
 
-                <p><strong>John Doe</strong> is your host</p>
+                <p><strong>{property.landlord.name}</strong> is your host</p>
 
             </div>
                 <hr />
                 <p className="mt-6 text-lg">
-                   Lorem ipsum dolor sit amet. provident!
+                {property.description}
                 </p>
         </div>
-        <ReservationSideBar/>
+        <ReservationSideBar
+        
+        property = {property}
+        />
 
     </div>
 
